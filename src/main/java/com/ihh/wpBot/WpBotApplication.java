6c5931f419e54e734f70077a282b8b946b5221d4
Tahir Atakan Can <atkan4202@gmail.com>
2026-02-27 17:37:53 +0300

    Initial commit

---
+++ src/main/java/com/ihh/wpBot/WpBotApplication.java
@@ -0,0 +1,13 @@
+package com.ihh.wpBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ihh/wpBot/WpBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihh.wpBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
