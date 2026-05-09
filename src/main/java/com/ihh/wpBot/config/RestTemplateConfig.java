7579e7f81f91402cef87682abcacf9930a6299d0
Tahir Atakan Can <atkan4202@gmail.com>
2026-05-09 09:13:15 +0300

    fix: properly configure RestTemplate UTF-8 - remove old converters, disable ASCII escape

--- src/main/java/com/ihh/wpBot/config/RestTemplateConfig.java
+++ src/main/java/com/ihh/wpBot/config/RestTemplateConfig.java
@@ -1,8 +1,9 @@
 package com.ihh.wpBot.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
@@ -16,14 +17,19 @@ public class RestTemplateConfig {
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
 
+        restTemplate.getMessageConverters().removeIf(c -> c instanceof StringHttpMessageConverter);
         restTemplate.getMessageConverters().add(0,
                 new StringHttpMessageConverter(StandardCharsets.UTF_8));
 
-        for (HttpMessageConverter<?> converter : restTemplate.getMessageConverters()) {
-            if (converter instanceof MappingJackson2HttpMessageConverter jacksonConverter) {
-                jacksonConverter.setDefaultCharset(StandardCharsets.UTF_8);
-            }
-        }
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(JsonGenerator.Feature.ESCAPE_NON_ASCII);
+
+        MappingJackson2HttpMessageConverter jacksonConverter =
+                new MappingJackson2HttpMessageConverter(mapper);
+        jacksonConverter.setDefaultCharset(StandardCharsets.UTF_8);
+
+        restTemplate.getMessageConverters().removeIf(c -> c instanceof MappingJackson2HttpMessageConverter);
+        restTemplate.getMessageConverters().add(0, jacksonConverter);
 
         return restTemplate;
     }
